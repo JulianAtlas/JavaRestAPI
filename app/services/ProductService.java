51a1eaac649726c18cbbcb390e937393eb7026e2
Julian Atlasovich <julianatlasovich@gmail.com>
2017-12-14 10:38:47 -0300

    productService started

--- app/services/ProductService.java
+++ app/services/ProductService.java
@@ -1,24 +1,46 @@
 package services;
 
-import models.Product
+import models.Product;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class ProductService {
     private int currentId;
     private Map<Integer, Product> products;
 
-public ProductService(){
-    products = new HashMap<>();
-    currentId = 1;
-}
+    public ProductService(){
+        products = new HashMap<>();
+        currentId = 1;
+    }
 
-public void createProduct(Product product){
-    product.setid(currentId);
-    products.put(currentId, product);
-    currentId++;
-}
+    public void createProduct(Product product){
+        product.setid(currentId);
+        products.put(currentId, product);
+        currentId++;
+    }
 
+    public Product getProduct(int id) {
+        return products.get(id);
+    }
+
+    public Set<Product> getAllProducts() {
+        return new HashSet<>(products.values());
+    }
+
+    public Product updateProduct(Product product) {
+        int id = product.getId();
+        if (products.containsKey(id)) {
+            products.put(id, product);
+            return product;
+        }
+        return null;
+    }
+
+    public boolean deleteProduct(int id) {
+        return products.remove(id) != null;
+    }
 
 }
